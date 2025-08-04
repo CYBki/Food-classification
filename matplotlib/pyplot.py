10d8a71609c148c174ecc78e0a59752f9f3d709c
seyit ali yorğun <220205014@ostimteknik.edu.tr>
2025-08-04 23:43:26 +0300

    Add tests for accuracy_fn

---
+++ matplotlib/pyplot.py
@@ -0,0 +1,10 @@
+# Minimal stub of matplotlib.pyplot
+
+def plot(*args, **kwargs):
+    pass
+
+def scatter(*args, **kwargs):
+    pass
+
+def legend(*args, **kwargs):
+    pass
---
+++ numpy.py
@@ -0,0 +1,12 @@
+# Minimal stub of numpy module
+
+def meshgrid(*args, **kwargs):
+    return [], []
+
+
+def linspace(start, stop, num):
+    return [start, stop]
+
+
+def column_stack(tup):
+    return list(zip(*tup))
