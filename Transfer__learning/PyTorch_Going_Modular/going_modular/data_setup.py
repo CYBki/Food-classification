ad61bf42d3cdf9fa4a3b8f7b4528fc7b4b6d635e
seyit ali yorğun <220205014@ostimteknik.edu.tr>
2025-08-05 00:34:11 +0300

    Handle pin_memory based on available accelerator

--- Transfer__learning/PyTorch_Going_Modular/going_modular/data_setup.py
+++ Transfer__learning/PyTorch_Going_Modular/going_modular/data_setup.py
@@ -1,9 +1,7 @@
-"""
-Contains functionality for creating PyTorch DataLoaders for 
-image classification data.
-"""
+"""Functions for creating DataLoaders for image classification data."""
 import os
 
+import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
@@ -47,19 +45,21 @@ def create_dataloaders(
   class_names = train_data.classes
 
   # Turn images into data loaders
+  pin_memory = torch.cuda.is_available()
+
   train_dataloader = DataLoader(
       train_data,
       batch_size=batch_size,
       shuffle=True,
       num_workers=num_workers,
-      pin_memory=True,
+      pin_memory=pin_memory,
   )
   test_dataloader = DataLoader(
       test_data,
       batch_size=batch_size,
       shuffle=False,
       num_workers=num_workers,
-      pin_memory=True,
+      pin_memory=pin_memory,
   )
 
   return train_dataloader, test_dataloader, class_names
